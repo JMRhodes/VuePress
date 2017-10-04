acc79d34220526b8d9d4e712510d4874d394bd04
Justin Rhodes <justin.m.rhodes@gmail.com>
2017-10-04 16:26:25 -0500

    updated theme files

--- app/themes/vuepress/index.php
+++ app/themes/vuepress/index.php
@@ -4,4 +4,17 @@
  *
  * @package AD Starter
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>{{ name }}</title>
+</head>
+<body>
+<div id="app"></div>
+<script src="/dist/build.js"></script>
+</body>
+</html>
 
